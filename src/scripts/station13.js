afd967da4a852ad21c430960c3d9e3b594059d68
キノ0510 <kinont0510@gmail.com>
2024-12-27 22:32:05 +0900

    section13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const checkbox = e.target; // チェックボックス要素
+  if (checkbox.checked) {
+    document.getElementById("text").style.backgroundColor = 'red'; // チェックされた場合
+  } else {
+    document.getElementById("text").style.backgroundColor = "white"; // チェックが外された場合
+  }
 }
